60eca4b6522f78c7c175d41c3776008f6993407f
Vincenzofdg <vincenzofdg.dev@gmail.com>
2024-01-09 03:20:58 -0300

    feat: fixed

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Routes from './Routes.jsx';
+import Routes from './Routes';
 
 import Global from "./Context/Global.jsx";
 import './Style/Main.css';
