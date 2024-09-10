a807d7b1476b1560dca272e4c4101cb7c29c5215
Vincenzo F. Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2024-09-10 08:54:54 -0300

    Update NotFound.jsx

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -14,7 +14,7 @@ function NotFound() {
     <div className="page grid-cointainer">
       <div className="error-container">
         <img className="error-icon" src={Error} alt="Page not Found" />
-        <p id="error-text">Something went wrong please go back to Home Page</p>
+        <p id="error-text">Something went wrong please go back to Home Page teste bood</p>
         <br />
         <p onClick={handleClick} id="go-back-text">click here ☕️</p>
       </div>
